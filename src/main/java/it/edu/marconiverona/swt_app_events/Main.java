cc39b7837812eafa66264027db6eb46866ed68ea
20550-DeFeliceGiulio <122974227+20550-DeFeliceGiulio@users.noreply.github.com>
2023-04-19 12:47:53 +0200

    Add files via upload
    GridLayout + Handle and Declare

--- src/main/java/it/edu/marconiverona/swt_app_events/Main.java
+++ src/main/java/it/edu/marconiverona/swt_app_events/Main.java
@@ -9,6 +9,41 @@ import org.eclipse.swt.widgets.Listener;
 
 public class Main {
 
+    public static void bottoneOperazione(String operazione) throws NumberFormatException {
+        // ottieni gli elementi grafici dal repository view
+        Text myField01 = (Text) Repository.repository_View.get("myField01");
+        Text myField02 = (Text) Repository.repository_View.get("myField02");
+        Text myField03 = (Text) Repository.repository_View.get("myField03");
+        
+        // leggi il contenuto dei myField
+        int operatore1 = Integer.parseInt( myField01.getText() );
+        int operatore2 = Integer.parseInt( myField02.getText() );
+        
+        // ottieni l'elemento model per i calcoli
+        Operazione op_model = (Operazione) Repository.repository_Model
+                .get("operazioni_model");
+        
+        // calcola il risultato
+        int risultato = 0;
+        switch (operazione) {
+            case "somma":
+                risultato = op_model.somma(operatore1, operatore2);
+                break;
+            case "differenza":
+                risultato = op_model.differenza(operatore1, operatore2);
+                break;
+            case "moltiplicazione":
+                risultato = op_model.moltiplicazione(operatore1, operatore2);
+                break;
+            case "divisione":
+                risultato = op_model.divisione(operatore1, operatore2);
+                break;
+        }
+
+        // aggiorna il campo myLabel02
+        myField03.setText("" + risultato);
+    }
+    
     public static void main(String[] args) {
 
         //------------------------------------
@@ -16,19 +51,33 @@ public class Main {
         //------------------------------------
         Display display = new Display();
         Shell root = new Shell(display);
-        root.setLayout(new FillLayout(SWT.VERTICAL));
+        root.setLayout(new GridLayout(2, true));
 
         //---- Istanze oggetti grafici + model + control=> CallBack/Listener
         //------------------------------------
         // istanze degli oggetti grafici
         //------------------------------------
         Label myLabel01 = new Label(root, SWT.NORMAL);
-        myLabel01.setText("addendo 1");
-        Button myButton01 = new Button(root, SWT.NORMAL);
-        myButton01.setText("operazione");
+        myLabel01.setText("Operando 1");
         Text myField01 = new Text(root, SWT.BORDER);
-        myField01.setSize(10, 1);;
+        
+        Label myLabel02 = new Label(root, SWT.NORMAL);
+        myLabel02.setText("Operando 2");
         Text myField02 = new Text(root, SWT.BORDER);
+        
+        Button myButton01 = new Button(root, SWT.NORMAL);
+        myButton01.setText("somma");
+        Button myButton02 = new Button(root, SWT.NORMAL);
+        myButton02.setText("differenza");
+        Button myButton03 = new Button(root, SWT.NORMAL);
+        myButton03.setText("moltiplicazione");
+        Button myButton04 = new Button(root, SWT.NORMAL);
+        myButton04.setText("divisione");
+        
+        Label myLabel03 = new Label(root, SWT.NORMAL);
+        myLabel03.setText("Risultato");
+        Text myField03 = new Text(root, SWT.BORDER);
+        myField03.setEditable(false);
 
         // Operazioni sulla finestra principale
         root.pack();
@@ -43,9 +92,15 @@ public class Main {
             {
                 put("root", root);
                 put("myLabel01", myLabel01);
+                put("myLabel02", myLabel02);
+                put("myLabel03", myLabel03);
                 put("myButton01", myButton01);
+                put("myButton02", myButton02);
+                put("myButton03", myButton03);
+                put("myButton04", myButton04);
                 put("myField01", myField01);
                 put("myField02", myField02);
+                put("myField03", myField03);
             }
         };
 
@@ -71,22 +126,45 @@ public class Main {
                 = new Listener() {
             @Override
             public void handleEvent(Event e) {
-                // legge contenuto delle TextEdit myField01 e myField02
-                Text myField01 = (Text) Repository.repository_View.get("myField01");
-                String text1 = myField01.getText();
-
-                Text myField02 = (Text) Repository.repository_View.get("myField02");
-                String text2 = myField02.getText();
-
-                // recupera l'oggetto model per i calcoli
-                Operazione op_model = (Operazione) Repository.repository_Model
-                        .get("operazioni_model");
-
-                // aggiorna il campo  myFiel
-                myField02.setText(""
-                        + op_model.somma(Integer.parseInt(text1),
-                                Integer.parseInt(text2)));
-
+                try {
+                    bottoneOperazione("somma");
+                } catch (NumberFormatException exc) {
+                    myField03.setText("Errore");
+                }
+            }
+        };
+        
+        Listener myButton02_listener
+                = new Listener() {
+            @Override
+            public void handleEvent(Event e) {
+                try {
+                    bottoneOperazione("differenza");
+                } catch (NumberFormatException exc) {
+                    myField03.setText("Errore");
+                }
+            }
+        };
+        Listener myButton03_listener
+                = new Listener() {
+            @Override
+            public void handleEvent(Event e) {
+                try {
+                    bottoneOperazione("moltiplicazione");
+                } catch (NumberFormatException exc) {
+                    myField03.setText("Errore");
+                }
+            }
+        };
+        Listener myButton04_listener
+                = new Listener() {
+            @Override
+            public void handleEvent(Event e) {
+                try {
+                    bottoneOperazione("divisione");
+                } catch (NumberFormatException exc) {
+                    myField03.setText("Errore");
+                }
             }
         };
 
@@ -97,6 +175,9 @@ public class Main {
             {
                 // costruttore della classe, non dell'istanza
                 put("myButton01_listener", myButton01_listener);
+                put("myButton02_listener", myButton02_listener);
+                put("myButton03_listener", myButton03_listener);
+                put("myButton04_listener", myButton04_listener);
             }
         };
 
@@ -112,6 +193,12 @@ public class Main {
         //------------------------------------
         myButton01.addListener(SWT.MouseDown,
                 (Listener) Repository.repository_Listener.get("myButton01_listener"));
+        myButton02.addListener(SWT.MouseDown,
+                (Listener) Repository.repository_Listener.get("myButton02_listener"));
+        myButton03.addListener(SWT.MouseDown,
+                (Listener) Repository.repository_Listener.get("myButton03_listener"));
+        myButton04.addListener(SWT.MouseDown,
+                (Listener) Repository.repository_Listener.get("myButton04_listener"));
 
         //------------------------------------
         // finche'  la finestra non viene chiusa _
--- src/main/java/it/edu/marconiverona/swt_app_events/Operazione.java
+++ src/main/java/it/edu/marconiverona/swt_app_events/Operazione.java
@@ -1,14 +1,24 @@
 package it.edu.marconiverona.swt_app_events;
 
 public class Operazione {
-
+    
     public int somma(int a, int b) {
         return a + b;
     }
+    public int differenza(int a, int b) {
+        return a - b;
+    }
+    public int moltiplicazione(int a, int b) {
+        return a * b;
+    }
+    public int divisione(int a, int b) {
+        return a / b;
+    }
+
 
     // costruttore 
     public Operazione() {
-
+        
     }
 
 }
